a25cc7601025eaa487220ea550650600080ec69f
luiserh1 <luis.serranohernandez.1415@gmail.com>
2020-10-09 18:30:35 +0200

    Práctica1 completada

--- webgl/threejs/S1_Polilinea.js
+++ webgl/threejs/S1_Polilinea.js
@@ -10,24 +10,17 @@
 
 // SHADER DE VERTICES
 var VSHADER_SOURCE = 
-	"attribute vec4 posicion;							\n" + // Los atributos se pueden imponer desde el código fuente de la aplicación
-	" 													\n" +
-	"varying lowp vec4 vPos;							\n" + //"varying" es la forma de comunicar el shader de vértices con el de fragmentos
+	"attribute vec4 posicion;							\n" +
 	"void main() {										\n" +
 	"	gl_Position = posicion;							\n" + // gl_Position palabra reservada
 	"   gl_PointSize = 10.0;							\n" + // gl_PointSize palabra reservada
-	"   vPos = posicion;								\n" + // gl_PointSize palabra reservada
 	"}													\n";
 
 // SHADER DE FRAGMENTOS
 var FSHADER_SOURCE = 
-	"precision mediump float;								\n" + // Hace falta indicar la precisión de los floats para que funcione
-	"varying lowp vec4 vPos;								\n" + // Misma declaración para hacer efectiva la comunicación
-	"void main() {											\n" + // En el shader de fragmentos no hay atributos
-	"	vec4 absvPos = abs(vPos);							\n" + // Las posiciones pueden ser negativas, loconvertimos a valor absoluto. Ahora van de 0 a 1									
-	"	float tone = 1.0 - (absvPos[0] + absvPos[1]) / 2.0;	\n" + // 							
-	"	gl_FragColor = vec4( tone , tone, tone, 1.0);		\n" +							
-	"}														\n";
+	"void main() {										\n" + // En el shader de fragmentos no hay atributos
+	"	gl_FragColor = vec4( 1.0 , 0.0, 0.0, 1.0);		\n" +							
+	"}													\n";
 
 function main()
 {
@@ -65,16 +58,6 @@ function main()
 	// consiguiendo un enlace del script con el shader
 	var coordenadas = gl.getAttribLocation(gl.program, "posicion");
 
-	// Creación del buffer de vertices
-	var bufferVertices = gl.createBuffer();
-	// Activación del buffer
-	gl.bindBuffer(gl.ARRAY_BUFFER, bufferVertices);
-	// Enlazado del buffer
-	// Posicion
-	gl.vertexAttribPointer(coordenadas, 3, gl.FLOAT, false, 0, 0);
-	gl.enableVertexAttribArray(coordenadas);
-	// Color
-
 	// escuchar eventos de raton
 	canvas.onmousedown = function (evento) 
 	{
@@ -83,7 +66,7 @@ function main()
     console.log("Inicialización exitosa");
 }
 
-var clicks = [];
+var puntos = [];
 function click(evento, gl, canvas, coordenadas)
 {
 	// coordenadas del click
@@ -95,19 +78,22 @@ function click(evento, gl, canvas, coordenadas)
 	// conversion de coordebadas al sistema de webgl por defecto
 	// cuadrado de 2x2 centrado <-- ejercicio: demostrar la fórmula
 	x = ((x - rect.left) - canvas.width/2) * 2 / canvas.width;
-	y = (canvas.height / 2 - (y - rect.top)) * 2 / canvas.height;
+	y = (canvas.height / 2 - (y - rect.top)) * 2/ canvas.height;
 
 	// guardar coordenadas
-	clicks.push(x); clicks.push(y); clicks.push(0.0);
-	// Los buffers data requieren trabajar con arrays tipados, no vale con el por defecto de JS
-	var puntos = new Float32Array(clicks);
+	puntos.push(x); puntos.push(y);
 
 	// borrar coordenadas
 	gl.clear(gl.COLOR_BUFFER_BIT);
 
-	// Rellena el BO con las coordenadas y lo manda a proceso
-	gl.bufferData(gl.ARRAY_BUFFER, puntos, gl.STATIC_DRAW);
-	gl.drawArrays(gl.LINE_STRIP, 0, puntos.length/3);
-	gl.drawArrays(gl.POINTS, 0, puntos.length/3);
+	// insertar las coordenadas como atributo y dibujarlos uno a uno
+	// WARNING -> Version sencilla, pero nada eficiente. Revisar documentacion
+	// Usar un buffer para meter todas las coordenadas y dar la orden de dibujo
+	// UNA SOLA VEZ es mucho más eficiente, permite el dibujado con paralelización
+	for (var i = 0; i < puntos.length; i += 2)
+	{
+		gl.vertexAttrib3f( coordenadas, puntos[i], puntos[i+1], 0.0);
+		gl.drawArrays(gl.POINTS, 0, 1);
+    }
     console.log("Click en (" + x + ", " + y + ") según el cubo canónico\n");
 }
